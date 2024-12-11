fc316f4d36b279d46f4d993e79928b5b364df788
Josh Payette <hello@joshpayette.dev>
2024-12-10 21:49:44 -0500

    mantine is now working

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,29 @@
-import "./styles.css";
+import '@mantine/core/styles.css';
+import { HeaderMenu } from "./header";
+
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+
+export const metadata = {
+  title: 'My Mantine app',
+  description: 'I have followed setup instructions carefully',
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}): JSX.Element {
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>
+          <HeaderMenu />
+          {children}
+          </MantineProvider>
+      </body>
     </html>
   );
 }
