26327592fd132a84906a1f6fe6e1680890cf2428
Josh Payette <hello@joshpayette.dev>
2024-12-24 16:38:41 -0500

    Updated the logo

--- packages/ui/src/logo/index.tsx
+++ packages/ui/src/logo/index.tsx
@@ -1,13 +1,27 @@
 import Image from 'next/image';
 import { getImagePath } from '../utils/get-image-path';
+import classes from './index.module.css';
 
 export function Logo({ width, height }: { width: number; height: number }) {
   return (
-    <Image
-      src={getImagePath({ app: 'ygt', path: 'general/logo.png' })}
-      alt="Logo of a purple and yellow toolbox."
-      width={width}
-      height={height}
-    />
+    <div className={classes.container}>
+      <Image
+        src={getImagePath({ app: 'ygt', path: 'general/logo.png' })}
+        alt="Logo of a purple and yellow toolbox."
+        width={width}
+        height={height}
+        loading="eager"
+        priority
+        className={classes.image}
+      />
+      <div className={classes.textContainer}>
+        <span>YOUR</span>
+        <span className={classes.accentText}>GAMING</span>
+        <div>
+          <span>TOOLKIT</span>
+          <span className={classes.dotCom}>.com</span>
+        </div>
+      </div>
+    </div>
   );
 }
