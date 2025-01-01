f21b7a8600e37c4ca45afaa1b5c96df6c2429c9a
Josh Payette <hello@joshpayette.dev>
2024-12-31 22:47:51 -0500

    Added UserProfile to database schema

--- app/auth.ts
+++ app/auth.ts
@@ -12,6 +12,9 @@ declare module 'next-auth' {
     user: {
       id: string;
       displayName: string;
+      bio: string;
+      avatarId: string;
+      image: string;
       role: string;
       /**
        * By default, TypeScript merges new interface properties and overwrites existing ones.
@@ -31,5 +34,36 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       apiKey: process.env.AUTH_RESEND_KEY,
       from: 'no-reply@yourgamingtoolkit.com'
     })
-  ]
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      const userProfile = await prisma.userProfile.upsert({
+        where: {
+          userId: user.id
+        },
+        update: {},
+        create: {
+          displayName: user.name ?? 'Traveler',
+          bio: 'No bio set yet.',
+          avatarId: '', // TODO: Add default avatar
+          user: {
+            connect: {
+              id: user.id
+            }
+          }
+        }
+      });
+
+      session.user = {
+        ...session.user,
+        id: user.id,
+        displayName: userProfile.displayName,
+        bio: userProfile.bio,
+        avatarId: userProfile.avatarId,
+        image: userProfile.image ?? ''
+      };
+
+      return session;
+    }
+  }
 });
